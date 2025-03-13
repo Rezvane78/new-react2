7f6c2bc996146761f538e3defacf77005151d926
rezvan <rezvane@gmail.com>
2025-03-13 11:59:27 +0330

    rezvan

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h2>home</h2>
       <h2>Iran</h2>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error et, numquam illo laborum praesentium quam reiciendis expedita voluptatibus assumenda, molestias est, eos ipsa earum aperiam nam? Fuga rerum commodi incidunt?</h2>
     </div>
   )
 }
